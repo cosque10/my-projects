3515e78832da4c32fda4ae1613a7249419145003
cosque10 <56222378+cosque10@users.noreply.github.com>
2019-10-14 22:23:03 +0100

    MobileDrinkingGameFIRSTCOMMIT

---
+++ MobileDrinkingGame/Assets/Code/Scripts/UIScripts/AddPlayers.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPlayers : MonoBehaviour
+{
+    public GameObject playersHolder;
+
+
+    public void AddBoutton()
+    {
+        for(int i = 0;i< playersHolder.transform.childCount; i++)
+        {
+            if (!playersHolder.transform.GetChild(i).gameObject.activeSelf)
+            {
+                playersHolder.transform.GetChild(i).gameObject.SetActive(true);
+                return;
+            }
+        }
+    }
+
+
+
+
+}
